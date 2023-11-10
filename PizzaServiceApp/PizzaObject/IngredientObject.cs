6d2e9df085192fbc87f91656f9c3be5ad36c603a
SBonhoff <SBonhoff@LINVM0075>
2023-11-10 11:41:49 +0100

    changed file Name PizzaTypes to PizzaObject created object file IngredientObject

---
+++ PizzaServiceApp/PizzaObject/IngredientObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaServiceApp.PizzaObject
+{
+    internal class IngredientObject
+    {
+        int ingredientID;
+        string ingredientName;
+
+        IngredientObject(int id, string name)
+        {
+            this.ingredientID = id;
+            this.ingredientName = name;
+        }
+    }
+}
---
+++ PizzaServiceApp/PizzaObject/PizzaObject.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaServiceApp.PizzaObject
+{
+    internal class PizzaObject
+    {
+        int pizzaID;
+        string pizzaName;
+
+        PizzaObject(int pizzaID, string pizzaName)
+        {
+            pizzaID = this.pizzaID;
+            pizzaName = this.pizzaName;
+        }
+    }
+}
