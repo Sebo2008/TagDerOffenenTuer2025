00b5157572b60bf0d6159d72e812d887f964df02
SBonhoff <SBonhoff@LINVM0075>
2023-11-10 12:13:54 +0100

    created standartPizzasList.txt added Hashset "stanadartPizzasList" with Pizza Names from standartPizzasList.txt

--- PizzaServiceApp/PizzaObject/PizzaObject.cs
+++ PizzaServiceApp/PizzaObject/PizzaObject.cs
@@ -16,6 +16,6 @@ namespace PizzaServiceApp.PizzaObject
             pizzaID = this.pizzaID;
             pizzaName = this.pizzaName;
         }
-        List<PizzaObject> standartPizzas;
+        readonly HashSet<string> standartPizzasList = File.ReadAllLines("standartPizzasList.txt").ToHashSet();
     }
 }
