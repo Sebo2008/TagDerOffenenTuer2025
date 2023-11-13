e07001af85c2348bd149abd0e2a365d6a81f41ba
Arne Weitschat <arneweitschat.w1@gmaill.com>
2023-11-13 11:17:04 +0100

    enter new names

---
+++ PizzaServiceApp/Forms/ShopForm.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaServiceApp.Forms
+{
+    public partial class ShopForm : Form
+    {
+        public ShopForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PizzaListe_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
