bc805edfc41aba068a3f72d5f30104305f82d283
SBonhoff <SBonhoff@LINVM0075>
2023-11-14 10:31:33 +0100

    Implemented Initialize Component in Discounts

---
+++ PizzaServiceApp/Discounts.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PizzaServiceApp
+{
+    public class Discounts
+    {
+        public static List<string> cuponCodes = new();
+        public static void InitializeCuponCodes()
+        {
+            foreach (string code in File.ReadAllLines("PizzaServiceApp/Discounts.txt"))
+            {
+                cuponCodes.Add(code);
+            }
+        }
+
+    }
+}
